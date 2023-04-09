098a3bcdeec17c6b61f12c929699b354336981bf
bhawanshbns <1ms19cs035@gmail.com>
2023-04-10 00:02:34 +0530

    BNS: config changes

---
+++ src/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MainContainer from  './MainContainer'
+const LandingPage = () =>{
+    return(
+        <>
+        <MainContainer />
+        </>
+    );
+};
+
+export default LandingPage;
