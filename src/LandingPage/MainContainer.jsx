f279cdf6baa49e0c86e61c5490bfe5d4443a93e7
bhawanshbns <1ms19cs035@gmail.com>
2023-04-09 23:51:04 +0530

    initial commit

---
+++ src/LandingPage/MainContainer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Button from "../components/Button";
+import styles from './LandingPageStyles.module.css';
+
+const MainContainer = () => {
+    return (
+      <div className={styles.maincontainer}>
+
+        <div>Grep E-Licensing</div>
+
+        <Button> <img src="photoCV.jpeg" /> </Button>
+
+        <div >New user?</div>
+        <Button> Sign Up</Button>
+        
+        <div>Already have an account?</div>
+        <Button>Sign in</Button
+        >
+      </div>
+    );
+  };
+
+export default MainContainer;
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function Footer() {
+  return (
+    <div >
+      <div >
+        <div>Learn</div>
+        <div>Support</div>
+        <div>About Us</div>
+        <div>Contact Us</div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
