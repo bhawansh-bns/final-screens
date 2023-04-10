62d743d5f238e5c3536e8b718aaef6c8232de5b9
bhawanshbns <1ms19cs035@gmail.com>
2023-04-10 15:39:00 +0530

    BNS: Footer updated

--- src/LandingPage/LandingPage.jsx
+++ src/LandingPage/LandingPage.jsx
@@ -8,15 +8,15 @@ import styles from './LandingPageStyles.module.css';
 
 const LandingPage = () =>{
     return(
-        <>
+        <div className={styles.landingpage}>
         <MainContainer />
         <Footer className={styles.footer}>
-        <Button className={styles.footerbutton}>Learn</Button>
-        <Button className={styles.footerbutton}>Support</Button>
-        <Button className={styles.footerbutton}>About Us</Button>
-        <Button className={styles.footerbutton}>Contact Us</Button>
-      </Footer>
-        </>
+            <Button className={styles.footerbutton}>Learn</Button>
+            <Button className={styles.footerbutton}>Support</Button>
+            <Button className={styles.footerbutton}>About Us</Button>
+            <Button className={styles.footerbutton}>Contact Us</Button>
+        </Footer>
+        </div>
     );
 };
 
