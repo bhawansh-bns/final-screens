94593cb4bf27fc1265076033c501dae007d2f7aa
bhawanshbns <1ms19cs035@gmail.com>
2023-04-11 22:12:42 +0530

    BNS: Code review and prepare for merge

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { useEffect } from "react";
+import LandingPage from "./LandingPage/LandingPage";
+import { Route, Routes } from "react-router-dom";
 import SignInPage from "./SignInPage/SignInPage";
 import SignUpPage from "./SignUpPage/SignUpPage";
 
 function App() {
+
   return (
-    <div>
-      <SignUpPage />
-    </div>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/sign-in" element={<SignInPage />} />
+      <Route path="/sign-up" element={<SignUpPage />} />
+
+    </Routes>
   );
 }
-
 export default App;
