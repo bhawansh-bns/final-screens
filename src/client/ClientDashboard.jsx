9b136ac426c02538a54058ea9b0a198a561e37f3
bhawanshbns <1ms19cs035@gmail.com>
2023-04-21 15:45:17 +0530

    Client dashboard

--- src/client/ClientDashboard.jsx
+++ src/client/ClientDashboard.jsx
@@ -1,13 +1,29 @@
 import React, { useState } from "react";
 import PageTemplate from "../components/template/PageTemplate";
-import AllApplicationListing from "./AllApplicationListing";
-import TotalApplications from "./TotalApplications";
 import UploadFiles from "./UploadFiles";
 import styles from './ClientDashboardStyles.module.css'
 
 const ClientDashboard = () => {
  
   const [applicationCount, setApplicationCount] = useState(0);
+  const applications = [
+    {
+      "CompanyName": "Test",
+      "Acceptance": "true",
+      "App Id": "5b1665d6-33c0-4207-86d7-53e3bbd09d09",
+      "Stage": "submitted",
+      "Admin": "Admin1",
+      "Applicant": "Applicant1"
+  },
+  {
+      "CompanyName": "Testhari",
+      "Acceptance": "true",
+      "App Id": "82abd784-c4e3-4182-af64-82d81d959b98",
+      "Stage": "submitted",
+      "Admin": "Admin1",
+      "Applicant": "Applicant1"
+  }
+  ]
 
 
   return (
@@ -15,11 +31,10 @@ const ClientDashboard = () => {
   
       <div className={styles.dashboardContainer}>
         <h2>Applicant Dashboard</h2>
-        <TotalApplications count={applicationCount} />
-
-        {/* <AllApplicationListing applications={applications} onDelete={handleFileDelete}/> */}
+       
 
         <UploadFiles />
+        
       </div>
 
     </PageTemplate>
--- src/client/UploadFiles.js
+++ src/client/UploadFiles.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import UploadForm from './UploadForm';
 import UploadedFile from './UploadedFile';
+import TotalApplications from "./TotalApplications";
+import ListApplications from "./ListApplications";
 import axios from 'axios';
 
 const UploadFiles = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [uploadedFile, setUploadedFile] = useState(null);
+  const [applications, setApplications] = useState([]);
 
   const onFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
@@ -13,20 +16,33 @@ const UploadFiles = () => {
 
   const onFileUpload = () => {
     const formData = new FormData();
-    formData.append('myFile', selectedFile, selectedFile.name);
-    axios.post('api/uploadfile', formData).then((res) => {
-      setUploadedFile(res.data);
+    formData.append('file', selectedFile);
+    axios.post('http://localhost:8443/sendApplication/Applicant1/Admin1/TesthariMtwat', formData).then((res) => {
+    console.log(res.data);
+    setUploadedFile(res.data);
     });
   };
-
+  const checkingEndpoints = () => {
+    axios.get('http://localhost:8443/getApplications/Applicant1').then((res) => {
+    console.log(res.data);
+    setApplications(res.data);
+    }).catch(err => console.error(err));
+  };
   return (
     <div>
+      <TotalApplications total={applications.length} />
       <UploadForm
         selectedFile={selectedFile}
         onFileChange={onFileChange}
         onFileUpload={onFileUpload}
       />
-      <UploadedFile uploadedFile={uploadedFile} />
+      {/* <UploadedFile uploadedFile={uploadedFile} /> */}
+      <button onClick={checkingEndpoints}>
+        See All Applications
+      </button>
+      <h3>My Application List</h3>
+      <ListApplications applications={applications} />
+      
     </div>
   );
 };
