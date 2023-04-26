346d33b8b84b98a8eb178a3a55731036ff038252
bhawanshbns <1ms19cs035@gmail.com>
2023-04-26 23:54:13 +0530

    BNS: Search component

--- src/client/UploadFiles.js
+++ src/client/UploadFiles.js
@@ -1,15 +1,26 @@
 import React, { useState, useEffect } from "react";
 import UploadForm from "./UploadForm";
 import UploadedFile from "./UploadedFile";
+import Search from "../components/search/Search";
 import axios from "axios";
+import {
+  useQuery,
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
 
 const UploadFiles = () => {
   const [selectedFile, setSelectedFile] = useState(null);
-  const [uploadedFile, setUploadedFile] = useState(null);
-  const [showPopup, setShowPopup] = useState(false);  
+  const [list, setList] = useState([]);
+  const [showPopup, setShowPopup] = useState(false);
   const [companyName, setCompanyName] = useState("");
   const [adminName, setAdmin] = useState("");
 
+  const queryClient = new QueryClient();
+
+  const handleAdminChange = (value) => {
+    setAdmin(value);
+  };
 
   const onFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
@@ -25,9 +36,21 @@ const UploadFiles = () => {
       )
       .then((res) => {
         console.log(res.data);
-        setUploadedFile(res.data);
+        setList(res.data);
       });
   };
+
+  const getAdmins = () => {
+    axios
+      .get(
+        `http://localhost:8443/getAdmins`
+      )
+      .then((res) => {
+        console.log(res.data);
+        list = res.data;
+      });
+  };
+
 
   const handleCompanyInputChange = (event) => {
     setCompanyName(event.target.value);
@@ -37,9 +60,9 @@ const UploadFiles = () => {
     setAdmin(event.target.value);
   };
 
-
   const handlePopup = () => {
     setShowPopup(!showPopup);
+    getAdmins();
   };
 
   return (
@@ -54,12 +77,17 @@ const UploadFiles = () => {
               onChange={handleCompanyInputChange}
               placeholder="Enter your Company Name..."
             />
-            <input
+            {/* <input
               type="text"
               value={adminName}
               onChange={handleAdminInputChange}
               placeholder="Choose your Admin..."
-            />
+            /> */}
+
+            <QueryClientProvider client={queryClient}>
+              <Search onAdminChange={handleAdminChange} list={list} />
+            </QueryClientProvider>
+
             <UploadForm
               selectedFile={selectedFile}
               onFileChange={onFileChange}
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,66 +1,30 @@
-import "./styles.css";
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
+import './SearchStyles.css';
+import React, { useState } from "react";
 
-const queryClient = new QueryClient();
+function Search(props) {
+  const {onAdminChange, list} = props;
 
-let reviewer_list = [];
-const options = {
-  method: "GET",
-  url: "http://localhost:8449/getReviewers",
-};
+  const [list1, setList] = useState([]);
 
-async function getData() {
-  axios
-    .request(options)
-    .then(function(response) {
-      reviewer_list = response.data;
-    })
-    .catch(function(error) {
-      console.error(error);
-    });
-}
-let data = getData();
+  setList(list);
 
-
-function Search() {
-  const { isLoading, error, data1 } = useQuery({
-    queryFn: () => axios.request(options),
-  });
-
-  console.log(data1);
+  // const [adminName, setAdmin] = useState("");
   const [value, setValue] = useState("");
-  const [reviewer, setReviewer] = useState("");
 
   const onChange = (event) => {
     setValue(event.target.value);
   };
   const onSearch = (searchTerm) => {
-    setReviewer(searchTerm);
+    // setAdmin(searchTerm);
     setValue(searchTerm);
-    // for(let k in dict){
-    //   if(dict[k]['name'].includes(searchTerm)){
-    //     setId(dict[k]['id'])
-    //     setName(dict[k]['name'])
-    //   }
-
-    //}
+    onAdminChange(searchTerm);
   };
-  // if (isLoading) return "Loading...";
 
-  // if (error) return "An error has occurred: " + error.message;
   return (
     <div className="App">
-      <h1>Search for Reviewer</h1>
       <div className="search-container">
         <div className="search-inner">
           <input
-            id="reviewer_name"
             type="text"
             value={value}
             onChange={onChange}
@@ -68,7 +32,7 @@ function Search() {
           <button onClick={() => onSearch(value)}>Search</button>
         </div>
         <div className="dropdown" style={{ width: "205px" }}>
-          {reviewer_list
+          {list1
             .filter((item) => {
               const searchTerm = value.toLowerCase();
               const item1 = item["Username"].toLowerCase();
@@ -90,8 +54,6 @@ function Search() {
             ))}
         </div>
       </div>
-      {/* <div>
-              <h2>{id?`(${id}: ${pok_nam})`:''}</h2></div>     */}
     </div>
   );
 }
